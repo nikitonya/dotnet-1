269295980cbe3bcf2073a6e16484aaa1e6d79bd8
nikitonya <nik_osipov_2014@mail.ru>
2022-02-16 10:42:41 +0400

    initial commit

---
+++ src/promproglab1/promproglab1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace promproglab1
+{
+    internal class Program
+    {
+        int myField;
+        static void Main(string[] args)
+        {
+             
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
