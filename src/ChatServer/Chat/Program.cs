b72d6f4b258a83192ac2d6a076919a7d53d8c622
nikitonya <nik_osipov_2014@mail.ru>
2022-04-23 14:50:38 +0400

    initial commit

---
+++ src/ChatServer/Chat/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Spectre.Console;
+using System;
+using System.Threading.Tasks;
+
+namespace Chat
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var userName = AnsiConsole.Ask<string>("What's your username");
+
+            var connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5000/chatroom")
+                .Build();
+
+            connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                AnsiConsole.MarkupLine($"[red]{user}[/]: [blue]{message} [/]");
+            });
+
+            await connection.StartAsync();
+            while (true)
+            {
+                var message = AnsiConsole.Ask<string>($"{userName}: ");
+
+                if (message == "exit") break;
+
+                connection.InvokeAsync("SendMessage", userName, message);
+            }
+
+
+            await connection.StopAsync();
+
+        }
+    }
+}
---
+++ src/ChatServer/ChatServer/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace ChatServer
+{
+    public class ChatHub : Hub
+    {
+        public Task SendMessage(string user, string message)
+        {
+            return Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
